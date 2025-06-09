ab1023cafd0e7977860aa52e99233e44bb6e9b99
Sophia Song <songfeiyah@163.com>
2025-06-09 10:34:14 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+def print_hello():
+    print("Hello")
+
+def print_world():
+    print("World")
+
+def main():
+    print_hello()
+    print_world()
+
+# Call the main function
+if __name__ == "__main__":
+    main()
